3f01b862770fbcd656fa73cab289cea900dfa950
Lucas Irineu <lucasirineu119@gmail.com>
2023-02-07 18:59:24 -0300

    requisito 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,1 +1,4 @@
-db.produtos.find({}, { _id: 0});
+db.produtos.find(
+  { ingredientes: { $elemMatch: { $eq: 'picles'} } },
+  { _id: 0, nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 }} 
+);
