2a78401e228681457623651403bf4a2897121c3b
Lucas Irineu <lucasirineu119@gmail.com>
2023-02-07 19:57:51 -0300

    requisito 16

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,1 +1,5 @@
-db.produtos.find({}, { _id: 0 });
+db.produtos.updateOne({
+  nome: "Big Mac",
+}, { $set: { ultimaModificacao: Date() } });
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
