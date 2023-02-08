18036399b3c2d561b9a77a57709365f330471acb
Lucas Irineu <lucasirineu119@gmail.com>
2023-02-07 21:00:33 -0300

    requisito 26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,1 +1,6 @@
-db.produtos.find({}, { _id: 0, nome: 1 });
+db.produtos.updateMany(
+  { "valoresNutricionais.2.percentual": { $gt: 20, $lt: 40 } }, 
+  { $push: { tags: { $each: ["contém sódio"] } } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
